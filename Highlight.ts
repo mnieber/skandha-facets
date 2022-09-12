6e5bed8d6933ba31dd5a8259426ed757bcb64df4
Maarten Nieber <hallomaarten@yahoo.com>
2022-09-12 16:59:45 +0200

    Use highlightItemDefaultCbs

--- Highlight.ts
+++ Highlight.ts
@@ -3,13 +3,17 @@ import { data, operation, output } from 'skandha';
 import { HighlightCbs } from './HighlightCbs';
 export type { HighlightCbs } from './HighlightCbs';
 
+const highlightItemDefaultCbs = (highlight: Highlight) => ({});
+
 export class Highlight<ValueT = any> {
   static className = () => 'Highlight';
 
   @data id: string | undefined;
   @output item?: ValueT;
 
-  @operation @host(['id']) highlightItem(id: string) {
+  @operation @host(['id'], highlightItemDefaultCbs) highlightItem(
+    id: string | undefined
+  ) {
     return (cbs: HighlightCbs['highlightItem']) => {
       this.id = id;
       cbs.scrollItemIntoView && cbs.scrollItemIntoView();
