295e3bb287d9843989014b10b96832d20f734371
Maarten Nieber <hallomaarten@yahoo.com>
2022-11-03 12:34:53 +0100

    Add dropDefaultCbs

--- DragAndDrop.ts
+++ DragAndDrop.ts
@@ -3,13 +3,23 @@ import { data, operation } from 'skandha';
 import { DragAndDropCbs } from './DragAndDropCbs';
 import { DropPositionT } from './Insertion';
 import { isBefore } from './internal/utils';
+import { selectionIsInsertedOnDragAndDrop } from './policies/selectionIsInsertedOnDragAndDrop';
+
 export type { DragAndDropCbs } from './DragAndDropCbs';
+
+const dropDefaultCbs = (dragAndDrop: DragAndDrop) => ({
+  drop: function (this: DragAndDropCbs['drop']) {
+    selectionIsInsertedOnDragAndDrop(dragAndDrop, this.dropPosition);
+  },
+});
 
 export class DragAndDrop {
   static className = () => 'DragAndDrop';
 
   @data hoverPosition?: DropPositionT;
-  @operation @host(['dropPosition']) drop(dropPosition: DropPositionT) {
+  @operation @host(['dropPosition'], dropDefaultCbs) drop(
+    dropPosition: DropPositionT
+  ) {
     return (cbs: DragAndDropCbs['drop']) => {
       cbs.drop();
     };
@@ -41,4 +51,20 @@ export class DragAndDrop {
       },
     };
   }
+}
+
+export type DragPropsT = {
+  draggable?: boolean;
+  onDragStart?: (event: any) => void;
+  onDragOver?: (event: any) => void;
+  onDragEnd?: (event: any) => void;
+  onDrop?: (event: any) => void;
+};
+
+export function dragState(hoverPosition: any, itemId: string) {
+  return hoverPosition?.targetItemId === itemId
+    ? hoverPosition?.isBefore
+      ? 'before'
+      : 'after'
+    : undefined;
 }
