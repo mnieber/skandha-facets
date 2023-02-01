8fecef7d9cd736028b1578b967f6c950d752cd07
Maarten Nieber <hallomaarten@yahoo.com>
2023-02-01 19:42:18 +0100

    Sort exports

--- policies/index.ts
+++ policies/index.ts
@@ -1,14 +1,16 @@
-export { highlightFollowsSelection } from "./highlightFollowsSelection";
-export { highlightIsCorrectedOnFilterChange } from "./highlightIsCorrectedOnFilterChange";
-export { newItemsAreAddedBelowTheHighlight } from "./newItemsAreAddedBelowTheHighlight";
-export { newItemsAreInsertedWhenConfirmed } from "./newItemsAreInsertedWhenConfirmed";
-export { newItemsAreCreatedAtTheTop } from "./newItemsAreCreatedAtTheTop";
-export { cancelNewItemOnHighlightChange } from "./cancelNewItemOnHighlightChange";
-export { insertionPreviewUsesDragSources } from "./insertionPreviewUsesDragSources";
-export { newItemsAreConfirmedOnEditingSave } from "./newItemsAreConfirmedWhenSaved";
-export { newItemsAreCancelledOnEditingCancel } from "./newItemsAreConfirmedWhenSaved";
-export { editingSetEnabled, editingSetDisabled } from "./newItemsAreEdited";
-export { filteringIsDisabledOnNewItem } from "./filteringIsDisabledOnNewItem";
-export { DragSourceFromNewItem } from "./DragSourceFromNewItem";
-export { selectionIsInsertedOnDragAndDrop } from "./selectionIsInsertedOnDragAndDrop";
-export { highlightNewItem } from "./highlightNewItem";
+export { cancelNewItemOnHighlightChange } from './cancelNewItemOnHighlightChange';
+export { DragSourceFromNewItem } from './DragSourceFromNewItem';
+export { filteringIsDisabledOnNewItem } from './filteringIsDisabledOnNewItem';
+export { highlightFollowsSelection } from './highlightFollowsSelection';
+export { highlightIsCorrectedOnFilterChange } from './highlightIsCorrectedOnFilterChange';
+export { highlightNewItem } from './highlightNewItem';
+export { insertionPreviewUsesDragSources } from './insertionPreviewUsesDragSources';
+export { newItemsAreAddedBelowTheHighlight } from './newItemsAreAddedBelowTheHighlight';
+export {
+  newItemsAreCancelledOnEditingCancel,
+  newItemsAreConfirmedOnEditingSave,
+} from './newItemsAreConfirmedOnEditingSaved';
+export { newItemsAreCreatedAtTheTop } from './newItemsAreCreatedAtTheTop';
+export { editingSetDisabled, editingSetEnabled } from './newItemsAreEdited';
+export { newItemsAreInsertedWhenConfirmed } from './newItemsAreInsertedWhenConfirmed';
+export { selectionIsInsertedOnDragAndDrop } from './selectionIsInsertedOnDragAndDrop';
