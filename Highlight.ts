a3fd35386406e46d77b4045ae927874624a461d4
Maarten Nieber <hallomaarten@yahoo.com>
2023-03-27 12:06:30 +0200

    Use HighlightUIConnectorT and highlightableIds

--- Highlight.ts
+++ Highlight.ts
@@ -1,13 +1,33 @@
-import { getCallbacks, host } from 'aspiration';
-import { data, operation, output } from 'skandha';
+import { getCallbacks, host, stub } from 'aspiration';
+import { data, input, operation, output } from 'skandha';
 import { HighlightCbs } from './HighlightCbs';
+import {
+  HighlightUIConnector,
+  HighlightUIConnectorOptionsT,
+} from './HighlightUIConnector';
 export type { HighlightCbs } from './HighlightCbs';
+export { highlightUIHandlers } from './HighlightUIConnector';
+export type {
+  HighlightUIConnectorOptionsT,
+  HighlightUIConnectorT,
+  HighlightUIPropsT,
+} from './HighlightUIConnector';
 
 const highlightItemDefaultCbs = (highlight: Highlight) => ({});
+
+const createUIConnectorDefaultCbs = (highlight: Highlight) => ({
+  createUIConnector: function (this: HighlightCbs['createUIConnector']) {
+    return new HighlightUIConnector({
+      highlight,
+      options: this.uiConnectorOptions,
+    });
+  },
+});
 
 export class Highlight<ValueT = any> {
   static className = () => 'Highlight';
 
+  @input highlightableIds: Array<string> = stub;
   @data id: string | undefined;
   @output item?: ValueT;
 
@@ -17,5 +37,12 @@ export class Highlight<ValueT = any> {
     const cbs = getCallbacks<HighlightCbs['highlightItem']>(this);
     this.id = id;
     cbs.scrollItemIntoView && cbs.scrollItemIntoView();
+  }
+
+  @host(['uiConnectorOptions'], createUIConnectorDefaultCbs) createUIConnector(
+    uiConnectorOptions?: HighlightUIConnectorOptionsT
+  ) {
+    const cbs = getCallbacks<HighlightCbs['createUIConnector']>(this);
+    return cbs.createUIConnector();
   }
 }
---
+++ HighlightUIConnector.ts
@@ -0,0 +1,135 @@
+import { Highlight } from './Highlight';
+import { pickNeighbour } from './internal/utils';
+
+export type HighlightUIConnectorOptionsT = {
+  useMouse?: boolean;
+  useKeys?: boolean;
+  itemSelector?: string;
+  onHighlightItem?: Function;
+  isKeyDown?: (e: any) => boolean;
+  isKeyUp?: (e: any) => boolean;
+  getHighlightableIds?: (e: any) => string[];
+};
+
+export interface HighlightUIConnectorT {
+  handle(itemId: string): HighlightUIPropsT;
+}
+
+export type PropsT = {
+  highlight: Highlight;
+  options?: HighlightUIConnectorOptionsT;
+};
+
+export class HighlightUIConnector implements HighlightUIConnectorT {
+  props: PropsT;
+  _handleKeyDown: any = undefined;
+  _handleMouseDown: any = undefined;
+
+  constructor(props: PropsT) {
+    this.props = props;
+    this._handleMouseDown =
+      props.options?.useMouse ?? true
+        ? this._createMouseDownHandler()
+        : undefined;
+    this._handleKeyDown =
+      props.options?.useKeys ?? true
+        ? this._createKeyDownHandler(props.options)
+        : undefined;
+  }
+
+  _highlight(e: any, itemId: string) {
+    this.props.highlight.highlightItem(itemId);
+    if (this.props.options?.onHighlightItem) {
+      this.props.options.onHighlightItem(e, itemId);
+    }
+  }
+
+  _createMouseDownHandler() {
+    return (e: any, itemId: string) => {
+      const isHighlighted = this.props.highlight.id === itemId;
+      if (!isHighlighted) {
+        this._highlight(e, itemId);
+      }
+    };
+  }
+
+  _createKeyDownHandler(options?: HighlightUIConnectorOptionsT) {
+    return (e: any, itemId: string, itemSelector: string) => {
+      const keysDown = ['ArrowDown'];
+      const keysUp = ['ArrowUp'];
+
+      const isDown = options?.isKeyDown
+        ? options?.isKeyDown(e)
+        : keysDown.includes(e.key);
+      const isUp = options?.isKeyUp
+        ? options?.isKeyUp(e)
+        : keysUp.includes(e.key);
+
+      if (isUp || isDown) {
+        e.preventDefault();
+        e.stopPropagation();
+        const highlightItemById = (itemId: any) => {
+          this._highlight({ ...e, ctrlKey: false }, itemId);
+        };
+
+        const highlightableIds = this.props.options?.getHighlightableIds
+          ? this.props.options?.getHighlightableIds(e)
+          : this.props.highlight.highlightableIds || [];
+
+        // Select the neighbour item
+        const newItemId = pickNeighbour(
+          highlightableIds,
+          this.props.highlight.id,
+          isDown,
+          highlightItemById
+        );
+
+        // Move the focus to the neighbour item. We assume that the
+        // list of UI elements reflects the list of selectable items (that
+        // is stored in highlight.selectableIds).
+        if (newItemId !== itemId) {
+          const idx = this.props.highlight.highlightableIds.indexOf(newItemId);
+          const nextElm = document.querySelectorAll(itemSelector)[idx];
+          // @ts-ignore
+          nextElm && nextElm.focus();
+        }
+      }
+    };
+  }
+
+  handle(itemId: any): HighlightUIPropsT {
+    const mouseHandlers =
+      this.props.options?.useMouse ?? true
+        ? {
+            onMouseDown: (e: any) => this._handleMouseDown(e, itemId),
+          }
+        : {};
+
+    const keyDownHandler =
+      this.props.options?.useKeys ?? true
+        ? {
+            onKeyDown: (e: any) =>
+              this._handleKeyDown(e, itemId, this.props.options?.itemSelector),
+          }
+        : {};
+
+    return {
+      isHighlighted: this.props.highlight.id === itemId,
+      ...mouseHandlers,
+      ...keyDownHandler,
+    };
+  }
+}
+
+export type HighlightUIPropsT = {
+  isHighlighted: boolean;
+  onMouseDown?: any;
+  onKeyDown?: any;
+};
+
+export function highlightUIHandlers<T extends HighlightUIPropsT>(props: T) {
+  return {
+    onMouseDown: props.onMouseDown,
+    onKeyDown: props.onKeyDown,
+  };
+}
