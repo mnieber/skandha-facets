5f650ee13111457ce1ddb8eb1cd529dbc13f6a8f
Maarten Nieber <hallomaarten@yahoo.com>
2023-03-27 12:05:10 +0200

    Use DeletionOptionsT

--- Deletion.ts
+++ Deletion.ts
@@ -1,12 +1,15 @@
 import { getCallbacks, host } from 'aspiration';
 import { operation } from 'skandha';
-import { DeletionCbs } from './DeletionCbs';
-export type { DeletionCbs } from './DeletionCbs';
+import { DeletionCbs, DeletionOptionsT } from './DeletionCbs';
+export type { DeletionCbs, DeletionOptionsT } from './DeletionCbs';
 
 export class Deletion {
   static className = () => 'Deletion';
 
-  @operation @host(['itemIds']) delete(itemIds: string[]) {
+  @operation @host(['itemIds', 'options']) delete(
+    itemIds: string[],
+    options?: DeletionOptionsT
+  ) {
     const cbs = getCallbacks<DeletionCbs['delete']>(this);
     return Promise.resolve(cbs.deleteItems());
   }
